8b0520a01eb28a3c73687d1e37244bdc743d50dd
Tadashi Ogino <tadashi.ogino@nifty.com>
2021-06-29 16:56:03 +0900

    Initial Commit

---
+++ OptionalTest/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  OptionalTest
+//
+//  Created by Tadashi Ogino on 2021/06/26.
+//
+
+import Foundation
+
+class A {
+    var services:[Int]?
+}
+
+var b: A
+
+b = A()
+// b.services = [1,2]
+print(b)
+print(b.services)
+for service in b.services! {
+    print(service)
+}
+print("Hello, World!")
+
